f0e60e8f25796713234bd8aeba7a72bf4968c691
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-06-18 13:49:12 +0300

    Update Oddsum.java

--- src/homework/homework_2/task_7/Oddsum.java
+++ src/homework/homework_2/task_7/Oddsum.java
@@ -1,6 +1,7 @@
 package homework.homework_2.task_7;
-
-public class Oddsum {public static void main(String [] args){
+// OddSum
+public class Oddsum {
+    public static void main(String [] args){
     int sumOfOddNumbers=0;
     for(int i=1; i<=100;i++){
         if(i%2==1){
