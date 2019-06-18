6758b9ed7e12555c05ac907f2532e06e36a6afed
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-06-18 13:40:36 +0300

    Update Sport.java

--- src/homework/homework_2/task_1/Sport.java
+++ src/homework/homework_2/task_1/Sport.java
@@ -3,8 +3,10 @@ package homework.homework_2.task_1;
 public class Sport {
     public static void main(String [] args){
         float dayFirst=10;
+        // float distanceSum=0;
         float distanceSum=10;
         for(int i=1; i<7;i++){
+            // поменяй эти строки местами
             dayFirst*=1.1;
             distanceSum+=dayFirst;
         }
